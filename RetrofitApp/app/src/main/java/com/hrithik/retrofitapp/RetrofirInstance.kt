24de394608a4ff090fbd0f895ac4df06345b8e2b
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-05-06 14:15:25 +0530

    added retrofit instance

---
+++ RetrofitApp/app/src/main/java/com/hrithik/retrofitapp/RetrofirInstance.kt
@@ -0,0 +1,18 @@
+package com.hrithik.retrofitapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofirInstance {
+
+    companion object{
+        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+        fun getRetrofitInstance(): Retrofit{
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
