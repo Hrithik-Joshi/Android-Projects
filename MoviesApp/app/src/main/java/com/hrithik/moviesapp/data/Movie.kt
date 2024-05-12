a9894d4d1f2f7172369bf50d7c3e9f14234440d1
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-05-12 14:38:40 +0530

    data class

---
+++ MoviesApp/app/src/main/java/com/hrithik/moviesapp/data/Movie.kt
@@ -0,0 +1,21 @@
+package com.hrithik.moviesapp.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("id")
+    val movieId: Int,
+
+    @SerializedName("overview")
+    val overview: String,
+
+    @SerializedName("poster_path")
+    val posterPath: String,
+
+    @SerializedName("release_date")
+    val releaseDate: String,
+
+    @SerializedName("title")
+    val title: String,
+
+    )
