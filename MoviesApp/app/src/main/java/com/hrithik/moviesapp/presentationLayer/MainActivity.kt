d9f78c4c0a1bcddf3429ef1c0c7fc30b1cc088ab
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-07-15 17:23:51 +0530

    getting movies to main activity

--- MoviesApp/app/src/main/java/com/hrithik/moviesapp/presentationLayer/MainActivity.kt
+++ MoviesApp/app/src/main/java/com/hrithik/moviesapp/presentationLayer/MainActivity.kt
@@ -1,15 +1,72 @@
 package com.hrithik.moviesapp.presentationLayer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hrithik.moviesapp.R
+import com.hrithik.moviesapp.databinding.ActivityMainBinding
+import com.hrithik.moviesapp.presentationLayer.di.Injector
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: ViewModelFactory
+    private lateinit var movieViewModel: MyViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MovieAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        (application as Injector).createMovieSubComponent().inject(this)
+
+        movieViewModel = ViewModelProvider(this, factory)[MyViewModel::class.java]
+
+        initRecyclerView()
+
     }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.recyclerView.adapter = adapter
+        displayPopularMovies()
+
+    }
+
+    private fun displayPopularMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer{
+
+            if (it !=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No Data Available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+
 }
